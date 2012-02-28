27a361897586925647b9a370ad47fc5a765857ca
Tristan Matthews <le.businessman@gmail.com>
2012-02-27 23:28:50 -0500

    using audiolib.js for sound rendering.

--- audio_callback.js
+++ audio_callback.js
@@ -1,42 +1,32 @@
-(function(){
+var flt;
+var counter = 0;
+var beat = 0;
+var ratio = 1;
+var impulse = 0;
+var bpm = 120.0;
+var INV_SECONDS_PER_MIN = 1.0 / 60.0;
+var meter = (1.0 / (bpm * INV_SECONDS_PER_MIN * (1.0 / 44100.0)));
 
-var ratio = 0;
-window.onkeypress = function() {
-    ++ratio;
-    if (ratio > 8) {
-        ratio = 1;
+function audioCallback(buffer, channelCount){
+    var period = Math.round(meter / ratio);
+    for (var i = 0; i < buffer.length; ++i)  {
+        while (counter >= meter) {
+            counter -= meter;
+            beat = 0;
+        }
+        if (counter % period == 0) {
+            impulse = 1;
+            ++beat;
+        }
+        buffer[i] = flt.pushSample(impulse);
+        impulse = 0;
+        ++counter;
     }
-    console.log(ratio);
-};
+}
 
-window.onload = function(){
-    var	dev;
-    var counter = 0;
-    var beat = 0;
-    var impulse = 0;
-    var bpm = 120.0;
-    var INV_SECONDS_PER_MIN = 1.0 / 60.0;
-    var meter = (1.0 / (bpm * (INV_SECONDS_PER_MIN) * (1.0 / 44100.0)));
-	var dev	= Sink(function(buffer){
-        var period = Math.round(meter / ratio);
-        for (var i = 0; i < buffer.length; ++i) {
-            while (counter >= meter) {
-                counter -= meter;
-                beat = 0;
-            }
-            if (counter % period == 0) {
-                // it's a beat
-                impulse = 1;
-                ++beat;
-            }
-            buffer[i] = impulse;
-            impulse = 0;
-            ++counter;
-		}
-	}, 1);
-
-	dev.on('error', function(e) {
-		console.error(e);
-	});
-};
-}());
+window.addEventListener('load', function(){
+    // Create an instance of the filter class
+    flt = audioLib.LP12Filter(44100, 1024, 5);
+    // Create an instance of the AudioDevice class
+    var dev = audioLib.AudioDevice(audioCallback /* callback for the buffer fills */, 1 /* channelCount */);
+}, true);
