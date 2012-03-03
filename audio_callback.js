bf440876322370795c2a5dc4ffb9139cac321fef
Tristan Matthews <le.businessman@gmail.com>
2012-03-03 18:21:55 -0500

    set BPM on the fly

--- audio_callback.js
+++ audio_callback.js
@@ -1,67 +1,78 @@
 
+var meters = [1, 2, 3];
 var tickers = new Array();
+var bpm_input;
 
-function toNumber(field, evt, ticker) {
+function clamp(min, max, value) { return Math.max(min, Math.min(max, value)); };
+
+function toNumber(field, evt, callback, min, max) {
   var theEvent = evt || window.event;
   var key = theEvent.keyCode || theEvent.which;
   keychar = String.fromCharCode(key);
   switch (key) {
     case  38: /* UP */
-        if (field.value < 99)
-            ++field.value;
-        ticker.setRatio(field.value);
+        ++field.value;
+        field.value = clamp(min, max, field.value);
+        callback(field.value);
         return true;
     case 40: /* DOWN */
-        if (field.value > 1)
-            --field.value;
-        ticker.setRatio(field.value);
+        --field.value;
+        field.value = clamp(min, max, field.value);
+        callback(field.value);
         return true;
     case 13: /* Enter */
-        ticker.setRatio(field.value);
+        field.value = clamp(min, max, field.value);
+        callback(field.value);
         return false;
   }
   if (("0123456789").indexOf(keychar) > -1)
       return true;
 }
 
-function Ticker(id, ratio) {
+function NumberInput(value, callback, min, max) {
+    var self = this;
+	this.input = document.createElement("input");
+    this.callback = callback;
+	this.input.type = "text";
+	this.input.value = value;
+	this.input.size = 3;
+	this.input.maxLength = 3;
+	this.input.style.width = "2em";
+	this.div = document.createElement("div");
+	this.div.appendChild(this.input);
+	document.getElementById('main').appendChild(this.div);
+	this.input.onkeypress = function(event) { return toNumber(self.input, event, self.callback, min, max); };
+};
+
+function Ticker(ratio) {
 	var self = this;
-    var SAMPLE_RATE = 44100.0;
-    this.id = id;
+    this.SAMPLE_RATE = 44100.0;
     this.counter = 0;
     this.beat = 0;
     this.ratio = ratio;
     this.nextSample = 0;
     this.bpm = 30.0;
     this.period = 0;
-	this.meter_input = document.createElement("input");
-	this.meter_input.type = "text";
-	this.meter_input.value = this.ratio;
-	this.meter_input.size = 2;
-	this.meter_input.maxLength = 2;
-	this.meter_input.style.width = "2em";
-	this.meter_input.onkeypress = function(event) { return toNumber(self.meter_input, event, self); };
-	this.div = document.createElement("div");
-	this.div.appendChild(this.meter_input); 
-	document.getElementById('main').appendChild(this.div);
 
-    this.flt = audioLib.LP12Filter(SAMPLE_RATE, this.ratio * 440, 4);
+    this.flt = audioLib.LP12Filter(this.SAMPLE_RATE, this.ratio * 440, 4);
     var INV_SECONDS_PER_MIN = 1.0 / 60.0;
-    this.meter = (1.0 / (this.bpm * INV_SECONDS_PER_MIN * (1.0 / SAMPLE_RATE)));
+    this.meter = (1.0 / (this.bpm * INV_SECONDS_PER_MIN * (1.0 / this.SAMPLE_RATE)));
     this.period = Math.round(this.meter / this.ratio);
-
-    this.setRatio = function(ratio) {
-        self.ratio = ratio;
-        self.setBPM(this.bpm);
-    }
 
     this.setBPM = function(newBPM) {
         if (newBPM > 0) {
             self.bpm = newBPM;
-            self.meter = 1.0 / (self.bpm * INV_SECONDS_PER_MIN * (1.0 / SAMPLE_RATE));
+            self.meter = 1.0 / (self.bpm * INV_SECONDS_PER_MIN * (1.0 / self.SAMPLE_RATE));
             self.period = Math.round(self.meter / self.ratio);
         }
     };
+
+    this.setRatio = function(ratio) {
+        self.ratio = ratio;
+        self.setBPM(self.bpm);
+    }
+
+	this.meter_input = new NumberInput(self.ratio, self.setRatio, 1, 99);
 
     this.tick = function() {
         while (self.counter >= self.meter) {
@@ -83,28 +94,28 @@ function audioCallback(buffer, channelCount){
     /* TODO: update tickers first, then process their output */
     for (var i = 0; i < buffer.length; ++i)  {
         sample = 0.0;
-        for (var t = 0; t < tickers.length; ++t) {
+        for (var t in tickers)
             sample += tickers[t].tick();
-        }
+
         buffer[i] = sample;
     }
 }
 
-/*
-window.addEventListener('click', function() {
-                                       var bpm = prompt("Input new bpm", "30");
-                                       bpm = parseInt(bpm);
-                                       for (var t = 0; t < tickers.length; ++t)
-                                            tickers[t].setBPM(bpm);
-                                       }, true);
-*/
+function setGlobalBPM(bpm) {
+    var bpm = parseInt(bpm);
+    for (var t in tickers)
+        tickers[t].setBPM(bpm);
+}
 
 window.addEventListener('load', function(){
-    // Create an instance of the filter class
-    var meters = [1, 2, 3];
-    for (var t = 0; t < 3; ++t) {
-        tickers[t] = new Ticker(t, meters[t]);
-    }
+    for (var t in meters)
+        tickers[t] = new Ticker(meters[t]);
+
+    var hr = document.createElement('hr');
+    document.getElementById('main').appendChild(hr);
+    setGlobalBPM(80);
+    bpm_input = new NumberInput(80, setGlobalBPM, 1, 500);
+
     // Create an instance of the AudioDevice class
-    var dev = audioLib.AudioDevice(audioCallback /* callback for the buffer fills */, 1 /* channelCount */);
+    var dev = audioLib.AudioDevice(audioCallback /* audio callback */, 1 /* channelCount */);
 }, true);
