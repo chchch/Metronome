28cde9ea013ac557a0a3b097a3df297f7ff7ea04
chuck <chuck@mixed-metaphors.com>
2012-03-03 19:13:00 -0500

    added play/pause button

--- metronome.js
+++ metronome.js
@@ -8,45 +8,45 @@ $(document).ready(function() {
     nomes = [];
     var five = new Nome(nome_size.w,nome_size.h,5);	
 	var four = new Nome(nome_size.w,nome_size.h,4);
-    master_nome = 0;
-    bpm = Math.round(master_bpm/nomes[master_nome].beats);
 	frame = 0;
-    max_frame = fpm/bpm;
+    master_nome = 0;
 
     $("#main").sortable({
         items: "div:not(.no-sort)",
         stop: function(event,ui) {
-                master_nome = $("#main div:eq(2)").attr('id').split("-")[1];
-                bpm = Math.round(master_bpm/nomes[master_nome].beats);
-                max_frame = fpm/bpm;
-                for(var n=0;n<nomes.length;n++) {
-                    nomes[n].init();
-                }
-            }
+                master_nome = $("#main .nome:eq(0)").attr('id').split("-")[1];
+                InitAll();
+             }
         });
-/*    $("#main").append("<div style='position:relative;margin: 0 \
-        auto;width:"+(nome_size.w+120)+"px'><form \
-        style='position:absolute;width:100%;height:"+(nome_size.h+6)+"px;top:-3px; \
-        border-width:1px 1px 1px 0;border-style:solid;border-color:black;left:35px'> \
-        <div style='position:absolute;right:0px'> \
-        <input type='text' style='width:2em' size='3' max_length='3' \
-        value='60'></input>BPM </div></form></div>"); */
     $("#main").append("<div class='no-sort' \
-        style='position:relative;margin: 0 auto;width:"+(nome_size.w+130)+"px'> \
+        style='position:relative;margin:0 auto;width:"+(nome_size.w+130)+"px'> \
         <form style='position:absolute;width:100%;height:"+(nome_size.h+7)+"px; \
         top:-3px; background: grey;left:35px'> \
         <div style='position:absolute;right:5px'> \
         <input id='bpm' type='text' style='width:2em' size='3' max_length='3' \
-        value='60'></input>BPM</div></form></div>");
+        value='60'></input>BPM</div> \
+        <div id='playpause' class='no-sort' \
+        style='position:absolute;right:0px;top:"+(nome_size.h+10)+"px;height:20px; \
+        '></div> \
+        </form></div>");
+    playpause = new PlayPause(nome_size.h/2);
+    playpause.play();
+    $("#playpause").append(playpause.canvas);
+    $("#playpause").click(function() {
+        if(!animator.active) {
+            animator.pause();
+            playpause.pause();
+        }
+        else {
+            animator.pause();
+            playpause.play();
+        }
+    });
     $("#bpm").keydown(function(event) {
         var result = numbersonly(document.getElementById("bpm"),event,999);
         if(result[1] != false) {
             master_bpm = result[1];
-            bpm = Math.round(master_bpm/nomes[master_nome].beats);
-            max_frame = fpm/bpm;
-            for(var n=0;n<nomes.length;n++) {
-                nomes[n].init();
-            }
+            InitAll();
         }
         return result[0];
     });
@@ -56,22 +56,34 @@ $(document).ready(function() {
 	$("#plus").click(function() {
 		var newnome = new Nome(nome_size.w,nome_size.h,2);
 		$("#plus").before(newnome.div);
+        newnome.init();
 		newnome.start();
 	});
+    InitAll();
 	five.start();
 	four.start();
-	animator.start();
+//	animator.start();
 
 });
 
+function InitAll() {
+    bpm = master_bpm/nomes[master_nome].beats;
+    max_frame = Math.round(fpm/bpm);
+    for(var n=0;n<nomes.length;n++) {
+        nomes[n].init();
+    }
+}
+
 Nome = function(w,h,beats) {
 	var self = this;
-    if(typeof nomes == "undefined") nomes = [];
+    nomes = nomes || [];
     nomes.push(this);
 	this.div = document.createElement("div");
     this.div.id = "nome-"+(nomes.length-1);
-	this.canvas = document.createElement("canvas");
+	this.div.className = "nome";
+    this.canvas = document.createElement("canvas");
 	this.options = document.createElement("form");
+    this.options.style.paddingRight = "5px";
 	this.playing = document.createElement("input");
 	this.playing.type = "checkbox";
 	this.playing.checked = true;
@@ -82,11 +94,11 @@ Nome = function(w,h,beats) {
 	this.rhythm.maxLength = 2;
 	this.rhythm.style.width = "2em";
 	this.rhythm.value = beats;
-	this.rhythm.onkeypress = function(event) {
-        var result = numbersonly(self.rhythm,event);
+	$(this.rhythm).keydown(function(event) {
+        var result = numbersonly(self.rhythm,event,99);
         if(result[1] != false) self.init(result[1]);
         return result[0];
-    }
+    });
 	this.options.appendChild(this.playing);
 	this.options.appendChild(this.rhythm);
 	this.div.appendChild(this.options); 
@@ -124,7 +136,7 @@ Nome = function(w,h,beats) {
 		}	
 		self.ctx.fillStyle = "red";
 		for(var n=0;n<=d;n++) {
-			self.ctx.fillRect(n*self.pixelsperbeat-1,0,2,self.size.h);
+			self.ctx.fillRect(n*self.pixelsperbeat,0,2,self.size.h);
 		}
 		self.ctx.fill();
 		self.ctx.fillStyle = "green";
@@ -153,10 +165,10 @@ Nome = function(w,h,beats) {
 		for(var n=0;n<self.beats;n++) {
 			self.count.push(Math.round(n*self.framesperbeat));
 		}
+        if(!animator.active) self.draw();
 	}
 		
 	this.start = function() {
-		self.init();
 		self.active = true;
 		animator.enqueue(self);
 	}
@@ -167,6 +179,37 @@ Nome = function(w,h,beats) {
 		}
 		else self.silent = false;
 	}
+}
+
+PlayPause = function(h) {
+    var self = this;
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = h;
+    this.canvas.height = h;
+    this.size = h;
+    this.canvas.style.width = h + "px";
+    this.canvas.style.height = h + "px";
+    this.ctx = this.canvas.getContext('2d');
+    this.color = "black";
+    this.play = function() {
+        self.ctx.clearRect(0,0,self.size,self.size);
+        self.ctx.fillStyle = self.color;
+        self.ctx.beginPath();
+        self.ctx.moveTo(0,0);
+        self.ctx.lineTo(self.size,self.size/2);
+        self.ctx.lineTo(0,self.size);
+        self.ctx.closePath();
+        self.ctx.fill();
+    }
+    this.pause = function() {
+        self.ctx.clearRect(0,0,self.size,self.size);
+        self.ctx.fillStyle = self.color;
+        self.ctx.fillRect(0,0,self.size/2.5,self.size);
+        self.ctx.fillRect(self.size,0,-self.size/2.5,self.size);
+    }
+    this.clear = function() {
+        self.ctx.clearRect(0,0,self.size,self.size);
+    }
 }
 
 PSound = function(n) {
@@ -198,6 +241,14 @@ function Animator() {
 			if(!self.timer) self.animate();
 		}
 	}
+    this.pause = function() {
+        if(!self.active) {
+            self.active = true;
+            if(!self.timer) self.animate();
+        } else {
+            self.stop();
+        }
+    }
 	this.stop = function() {
 		self.active = false;
 		self.timer = false;
